abc849c05a15aeec3adf99cb2336990df9a79bde
Vlad-Ishchuk <vladishuk1105@gmail.com>
2022-12-15 13:06:50 +0200

    Lab7

---
+++ Lab7/Lab7.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+double Average(int m[],int n)
+{
+	double sum = 0;
+	for (int i = 0; i < n; i++)
+		sum += m[i];
+	return sum / n;
+}
+
+int main()
+{
+	int size=0;
+	cout << "Enter the number of elements\n";
+	cin >> size;
+	int* a = new int[size];
+	cout << "Enter elements\n";
+	for (int i = 0; i < size; i++)
+		cin >> a[i];
+	cout << "The average value of the entered elements\n" << Average(a,size);
+	return 0;
+}
